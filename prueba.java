d185f8c6818b1de75e6735145cce315b991bc21a
JUAN ESTEBAN HUERTAS SOLANO <102707938+JUANHUERTAS25@users.noreply.github.com>
2022-07-07 07:44:28 -0500

    prueba4

--- prueba.java
+++ prueba.java
@@ -1,21 +1,6 @@
-import java.io.IOException;
-
 public class prueba
 {
     public static void main(String[] args) {
         System.out.println("Hola");
-        try 
-        {
-            Runtime.getRuntime().exec
-            ("cmd /c start cmd.exe /K \" cd C:\\Archivos de programa\\Git && start git && cd .. && cd pruebaaaa && git status\"");
-            
-        } 
-        
-        
-        catch (IOException ioe) 
-        {
-            System.out.println (ioe);
-        }
-    
     }
 }
